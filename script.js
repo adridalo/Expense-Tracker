4853aef3fcc4be6f07166bbd083944d47c1be555
adridalo <93151842+adridalo@users.noreply.github.com>
2022-10-01 18:51:41 -0400

    implemented event listener for delete button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let inputs = document.getElementsByTagName('input');
 let del_btn = document.createElement('button');
 del_btn.type = "button";
 del_btn.textContent = 'X';
+let body = document.body;
 let btn = document.querySelector('button');
 let tbody = document.getElementById('tbody');
 
@@ -12,8 +13,17 @@ function addNewExpense() {
         removeStockRow();
     }
     let tr = addHTMLElement('tr',null,tbody);
-    for(let i = 0; i < 3; i++) {
-        let td = addHTMLElement('td',inputs[i].value,tr); 
+    for(let i = 0; i <= 3; i++) {
+        if(i === 3) {
+            let btn = addHTMLElement('button','X',tr);
+            btn.setAttribute('type','submit');
+            btn.style.width = "30px";
+            btn.addEventListener('click',() => {
+                tr.remove();
+            });
+            break;
+        }
+        let td = addHTMLElement('td',inputs[i].value,tr);
     }
 }
 
